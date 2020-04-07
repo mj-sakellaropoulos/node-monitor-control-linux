f7b01ab398e7ec869e450ab1d54f89913965b3f1
Michael John Sakellaropoulos <michael.john.tech@gmail.com>
2020-04-07 17:26:16 -0400

    Implemented Basic Auth

--- src/Server.ts
+++ src/Server.ts
@@ -9,21 +9,52 @@ import { XSET } from './XSET';
 
 export class BasicServer {
     public httpServer: Server;
+    public user: string;
+    public pass: string;
 
     constructor(listenAddress: string, port: number, user: string, pass: string) {
         this.httpServer = createServer(this.requestCallback);
         this.httpServer.listen(port, listenAddress);
+        this.user = user;
+        this.pass = pass;
     }
 
     public requestCallback: RequestListener = (
         request: IncomingMessage,
         response: ServerResponse
     ) => {
+        console.log(`REQUEST : [${request.method}] ${request.url}`);
+        
+        //basic auth
+        response.setHeader('WWW-Authenticate', 'Basic realm=NodeMonitorControlLinux')
+        if(request.headers.authorization){
+            const auth = request.headers.authorization.trim().split(" ");
+            if(auth[0] === "Basic"){
+                const base64 = auth[1];
+                const buff = new Buffer(base64, 'base64');
+                const token = buff.toString('ascii').trim().split(":");
+                const user = token[0]
+                const pass = token[1];
+                if(user !== this.user || pass !== this.pass){
+                    response.statusCode = 401;
+                    response.end("Unauthorized.")
+                }
+            }else{
+                response.statusCode = 401;
+                response.end("Unauthorized.")
+            }
+        }else{
+            response.statusCode = 401;
+            response.end("Unauthorized.")
+        }
+        
+
         switch (request.url) {
             case '/dpms': {
                 if (request.method === 'GET') {
                     response.end(JSON.stringify(XSET.getMonitorStatus()));
                 } else if (request.method === 'POST') {
+                    
                     const data:any = [];
                     request.on('data', chunk => {
                         data.push(chunk);
